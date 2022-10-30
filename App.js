ac94bab14d9da9aecab892b2d743ddd39479bd01
Shaikh Irfan <shaikhji1998@gmail.com>
2022-10-30 23:45:03 +0530

    Changes in Every Section

--- App.js
+++ App.js
@@ -37,3 +37,21 @@ var swiper = new Swiper(".popular-content", {
         }
     }
 });
+
+// Show Video
+let playButton = document.querySelector('.play-movie');
+let video = document.querySelector('.video-container');
+let myvideo = document.querySelector('#myvideo');
+let closebtn = document.querySelector('.close-video');
+
+playButton.onclick = () => {
+    video.classList.add("show-video");
+    // Auto play when click on button
+    myvideo.play();
+}
+
+closebtn.onclick = () => {
+    video.classList.remove("show-video");
+    // pause on close
+    myvideo.pause();
+}
